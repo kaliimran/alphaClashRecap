58561d9bf4bbf0bf4dc800842aa030cdd6d35fc8
kaliimran <imrankhanjoy600@gmail.com>
2024-10-26 16:16:20 +0600

    js 50% work done

--- js/alpha.js
+++ js/alpha.js
@@ -1,4 +1,44 @@
 
+function handleKeyboard(events){
+    const playerPress = events.key;
+
+    const currentAlphabetElement = document.getElementById('currentAlphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+    if(playerPress === expectedAlphabet){
+        console.log('You got it');
+
+        const currentScoreElement = document.getElementById('currentScore');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+
+        const newScore = currentScore + 1;
+
+        currentScoreElement.innerText = newScore
+
+
+        removeBackgroundColor(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        // alert('Try Again Now')
+        const currentLifeElement = document.getElementById('currentLife');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+
+        const lifeUpdate = currentLife - 1;
+        currentLifeElement.innerText = lifeUpdate;
+
+        if(lifeUpdate === 0){
+            gameOver()
+        }
+    }
+}
+
+
+document.addEventListener('keyup', handleKeyboard)
 
 
 
@@ -6,8 +46,14 @@
 
 
 
+function continueGame(){
+    const alphabet = randomAlphabet();
 
+    const currentAlphabetElement = document.getElementById('currentAlphabet');
+    currentAlphabetElement.innerText = alphabet;
 
+    addBackgroundColor(alphabet);
+}
 
 
 
@@ -19,4 +65,15 @@
 function playNow(){
     setHideElement('firstSection');
     setUnHideElement('playSection');
+    continueGame();
+}
+
+function gameOver(){
+    setHideElement('playSection');
+    setUnHideElement('resultSection');
+}
+
+function playAgain(){
+    setUnHideElement('playSection');
+    setHideElement('resultSection');
 }
--- js/utility.js
+++ js/utility.js
@@ -6,3 +6,32 @@ function setUnHideElement (elementsId){
     const unHideElement = document.getElementById(elementsId);
     unHideElement.classList.remove('hidden');
 }
+
+function addBackgroundColor (elementsId){
+    const elements = document.getElementById(elementsId);
+    elements.classList.add('bg-orange-400');
+}
+function removeBackgroundColor (elementsId){
+    const elements = document.getElementById(elementsId);
+    elements.classList.remove('bg-orange-400');
+}
+
+
+
+
+
+
+// get a Generate random Alphabet
+
+function randomAlphabet (){
+    const randomAlphabetString = 'abcdefghijklmnopqrstuvwxyz'
+    const alphabets = randomAlphabetString.split('');
+
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+
+    return alphabet;
+
+}
